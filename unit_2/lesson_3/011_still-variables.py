28cdc02d3e07cfce7da6351483ac179b1a05dd19
Evan Zhang <evan.zhang25@ycdsbk12.ca>
2023-02-14 10:17:53 -0500

    completed 2.3 practice

---
+++ unit_2/lesson_3/011_still-variables.py
@@ -0,0 +1,4 @@
+full_name = "Juan Valdez"
+graduation_year = 2022
+
+print(f"My name is {full_name} and I'll graduate in {graduation_year}.")
---
+++ unit_2/lesson_3/012_your-schedule.py
@@ -0,0 +1,28 @@
+course1 = "Mathematics G10"
+course2 = "English G10"
+course3 = "World Religions G11"
+course4 = "French G10"
+course5 = "Pre-IB Functions"
+course6 = "Computer Studies G10"
+course7 = "History G10"
+course8 = "Science G10"
+
+teacher1 = "Mr. Zmak"
+teacher2 = "Ms. Laviolette"
+teacher3 = "Ms. Galucci"
+teacher4 = "Ms. Morra"
+teacher5 = "Mr. Bisogno"
+teacher6 = "Mr. Gallo"
+teacher7 = "Mr. Wood"
+teacher8 = "Mr. Howland"
+
+print("+------------------------------------------------------------+")
+print("| 1 | " + f"{course1:>36}" + " | " + f"{teacher1:>15}" + " |")
+print("| 2 | " + f"{course2:>36}" + " | " + f"{teacher2:>15}" + " |")
+print("| 3 | " + f"{course3:>36}" + " | " + f"{teacher3:>15}" + " |")
+print("| 4 | " + f"{course4:>36}" + " | " + f"{teacher4:>15}" + " |")
+print("| 5 | " + f"{course5:>36}" + " | " + f"{teacher5:>15}" + " |")
+print("| 6 | " + f"{course6:>36}" + " | " + f"{teacher6:>15}" + " |")
+print("| 7 | " + f"{course7:>36}" + " | " + f"{teacher7:>15}" + " |")
+print("| 8 | " + f"{course8:>36}" + " | " + f"{teacher8:>15}" + " |")
+print("+------------------------------------------------------------+")
